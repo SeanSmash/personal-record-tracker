341ffef4ce138c7ffac10e7edca499457c2dabdb
Sean Sarokas <sps4462@gmail.com>
2023-04-16 15:08:45 -0400

    create categories migration

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_16_190455) do
+ActiveRecord::Schema.define(version: 2023_04_16_190732) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "category"
+  end
 
   create_table "exercises", force: :cascade do |t|
     t.string "exercise"

14542e3248315c4d637e16221c2808f453e1fe9d
Sean Sarokas <sps4462@gmail.com>
2023-04-16 15:59:35 -0400

    creates exercise routes

---
+++ backend/app/controllers/application_controller.rb
@@ -0,0 +1,26 @@
+require 'pry'
+
+class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+  
+  # Add your routes here
+  get "/" do
+    { message: "Good luck with your project!" }.to_json
+  end
+
+  get "/exercises" do
+    exercises = Exercise.all 
+    exercises.to_json
+  end
+
+  get "/exercises/:id" do
+    exercise = Exercise.find(params[:id])
+    exercise.to_json
+  end
+
+  get "/exercises/:id/categories" do
+    exercise = Exercise.find(params[:id])
+    exercise.categories.all.to_json
+  end
+
+end
